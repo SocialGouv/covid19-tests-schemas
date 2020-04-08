4a031db4c3bf1f97850b54e1e44c06b700191148
Julien Bouquillon <julien.bouquillon@sg.social.gouv.fr>
2020-04-08 12:59:36 +0200

    fix(CI): set auto updates as chore

--- scripts/commit-docs.sh
+++ scripts/commit-docs.sh
@@ -7,6 +7,6 @@ if ! git diff --quiet docs/index.html; then
   git config user.email "45039513+SocialGroovyBot@users.noreply.github.com"
   git add docs/index.html
   git add openapi3.json
-  git commit -m "fix: update documentation from YAML"
+  git commit -m "chore: update documentation from YAML"
   git push --no-verify
 fi
